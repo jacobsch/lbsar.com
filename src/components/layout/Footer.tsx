c42c5f6c0e5242999942d28c2729e189c79d2887
Aria Bahrami <223552048+aria-bahrami@users.noreply.github.com>
2025-12-18 16:00:20 -0800

    Finished Footer and optimized for mobile view

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,4 @@
-import { Facebook, Instagram, Linkedin } from 'react-feather';
+import { Facebook, Instagram } from 'react-feather';
 
 import ArrowLink from '../ui/links/ArrowLink';
 
@@ -6,58 +6,62 @@ export default function Footer() {
   return (
     // TODO: Put section classname into Section component
     <section
-      className='relative inset-x-0 bottom-0 z-50 w-full bg-[#242424] px-2 pb-6	pt-12 text-center text-white'
+      className='relative inset-x-0 bottom-0 z-50 w-full bg-[#242424] px-2 pb-6 pt-12 text-center text-white'
       id='contact-us'
     >
       <div className='h2 container-heading pb-10'>QUICK LINKS</div>
-      <div className='grid grid-cols-2 gap-5'>
-        <div className='grid-rows grid gap-y-3'>
+
+      {/* Use 1 column on mobile, 3 columns on sm+ so message sits between sections */}
+      <div className='container mx-auto grid grid-cols-1 sm:grid-cols-3 gap-5 items-start'>
+        {/* Left column */}
+        <div className='sm:col-span-1 flex flex-col items-center text-center'>
           <div className='h3 container-heading'>CONNECT WITH US</div>
-          <div>
-            <ArrowLink
-              href='google maps link'
-              className='navbar-link'
-            >
-              {' '}
-              Address:
-              <br />
-              Sesame Street
-              <br />
-              City + Postal Code
+
+          <div className='mt-3 w-full'>
+            <ArrowLink href='https://maps.app.goo.gl/v8aocasQ4Y6N7uCU8' className='navbar-link' label='Address'>
+              <address className='not-italic text-center'>
+                <span className='block font-semibold'>Mailing Address:</span>
+                <span className='block'>PO Box 629</span>
+                <span className='block'>Lions Bay, BC V0N 2E0</span>
+              </address>
             </ArrowLink>
           </div>
-          <div>
-            <ArrowLink
-              href='mailto:replace@example.com'
-              className='navbar-link'
-            >
-              Email: replace@example.com
-              {/* TODO: Make this safe from webscraping */}
+
+          <div className='mt-3 w-full'>
+            <ArrowLink href='mailto:info@lbsar.com' className='navbar-link' label='Email'>
+              Email: info@lbsar.com
             </ArrowLink>
           </div>
         </div>
-        <div className='flex flex-col'>
-          <div className='h3 container-heading pb-8'>SOCIAL MEDIA</div>
-          <div className='flex items-center justify-center'>
-            <a
-              href='https://www.instagram.com/lionsbaysar/'
-              className='icon-link rounded-full border-2 p-2'
-            >
-              <Instagram />
-            </a>
-            <a
-              href='https://www.facebook.com/LionsBaySAR/'
-              className='icon-link mx-6 rounded-full border-2 p-2 sm:mx-12'
-            >
-              <Facebook />
-            </a>
 
-            <a
-              href='https://www.linkedin.com/company/example.com/'
-              className='icon-link rounded-full border-2 p-2'
-            >
-              <Linkedin />
-            </a>
+        {/* Center message (visually between the two sections) */}
+        <div className='sm:col-span-1 flex items-center justify-center'>
+          <div
+            className='bg-red-600 text-white rounded px-4 py-2 text-sm font-semibold text-center'
+            role='alert'
+          >
+            For All Search and Rescue Emergencies, call 911
+          </div>
+        </div>
+
+        {/* Right column */}
+        <div className='sm:col-span-1 flex flex-col items-center text-center'>
+          <div className='h3 container-heading pb-2'>SOCIAL MEDIA</div>
+          <div className='flex items-center justify-center'>
+            <div className='flex items-center justify-center space-x-6'>
+              <a
+                href='https://www.instagram.com/lionsbaysar/'
+                className='icon-link rounded-full border-2 p-2'
+              >
+                <Instagram />
+              </a>
+              <a
+                href='https://www.facebook.com/LionsBaySAR/'
+                className='icon-link rounded-full border-2 p-2'
+              >
+                <Facebook />
+              </a>
+            </div>
           </div>
         </div>
       </div>
