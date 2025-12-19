786326bf17d444c1aaf031175c788d5fb7fd9ec2
Aria Bahrami <223552048+aria-bahrami@users.noreply.github.com>
2025-12-18 20:52:15 -0800

    Edited wording on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,26 +20,27 @@ export default function HomePage() {
         <BackgroundImage imgClass='bg-parallax-1' />
 
         <Section
-          header='Lions Bay Volunteer Search & Rescue Team'
-          body='Lions Bay Search and Rescue Society operates under the umbrella of the Emergency Management Climate Readiness (EMCR).
-          Our team provides wilderness and urban search and rescue services, primarily in the area from Horseshoe Bay to Porteau Cove.
-          Lions Bay Search and Rescue also assists search and rescue teams in nearby regions as well as participating in disaster relief efforts across the province.'
+          body='Lions Bay Search and Rescue (LBSAR) is a volunteer emergency response organization providing search and rescue services in the 
+          Sea-to-Sky corridor.  Our response area includes the village of Lions Bay, the mountains of the Howe Sound Crest, and remote wilderness 
+          terrain that reaches into the Capilano watershed. The most popular trails we support include the Howe Sound Crest Trail which leads to the 
+          iconic Lions, Mount Brunswick, Mount Harvey, Deeks Lakes, and the Tunnel Bluffs lookout trail. We also provide mutual aid assistance to SAR 
+          teams in all areas of British Columbia, and have also been requested for Canada-wide SAR assistance. '
         />
 
         <BackgroundImage imgClass='bg-parallax-2' />
 
         <Section
-          body='Our team members are dedicated volunteers who are on call 24 hours a day and are trained and equipped to respond in any weather conditions.
-          Searches can range from urban calls to look for missing adults or children, to wilderness searches involving specialized high angle rope equipment and techniques.
-          Each search is directed by experienced search managers with a goal of finding missing persons and extracting them safely and as quickly as possible.'
+          body='We assist people who are lost, missing, injured, or in distress in a wide range of environments. 
+          Incidents may involve urban searches, lost or injured hikers, or complex rescues in steep and technical terrain.'
         />
 
         <BackgroundImage imgClass='bg-parallax-3' />
 
         <Section
-          body='Our team members are all highly skilled in urban and wilderness search techniques.
-          In addition to a mandatory basic training course, our members attend weekly training sessions to ensure skills are kept up to date.
-          Specialized weekend courses put on by the Justice Institute help members to develop specialized search and rescue skills and techniques.'
+          body='Operating under Emergency Management and Climate Readiness (EMCR), LBSAR works closely with neighbouring search and rescue teams 
+          and other emergency services like the RCMP, Fire, and Ambulance. Our volunteers train year-round to maintain operational readiness and to 
+          respond effectively in all weather and terrain conditions. Search and rescue services in British Columbia are provided at no cost to the public. 
+          If you, or someone else requires emergency assistance, call 911 immediately.'
         />
 
         <BackgroundImage imgClass='bg-parallax-4' />
