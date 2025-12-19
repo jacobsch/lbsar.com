cd70bbc18cd773c2d9e06ae53e9523eddf2290a0
Jacob Schwartz <jacobsch@users.noreply.github.com>
2025-12-18 17:21:38 -0800

    Update eslint for next16

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    dirs: ['src'],
-  },
-
   reactStrictMode: true,
   swcMinify: true,
-
 };
 
 module.exports = nextConfig;
