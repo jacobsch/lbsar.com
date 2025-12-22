69b66f865587fa6579b3b569b30f65af3a9e16f3
Aria Bahrami <223552048+aria-bahrami@users.noreply.github.com>
2025-12-21 18:56:52 -0800

    header redo for education drop down

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,58 +1,35 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import { List, Menu } from 'react-feather';
+import { List, Menu, ChevronDown } from 'react-feather';
 
 import UnderlineLink from '@/components/ui/links/UnderlineLink';
 import logo from '~/images/logo.png';
 
+/* -------------------- NAV DATA -------------------- */
 const links = [
   { href: '/', label: 'Home' },
   { href: '/#about-1', label: 'About Us' },
   { href: '/history', label: 'History' },
   { href: '/volunteer', label: 'Volunteer' },
   { href: '/donate', label: 'Donate' },
-  { href: '/education', label: 'Education' },
 ];
 
-const HeaderItem = ({ href, label }: { href: string; label: string }) => {
-  return (
-    <UnderlineLink
-      href={href}
-      className='navbar-link navbar-text-black ms-4 h-10 w-fit lg:ms-0 lg:h-auto lg:w-auto'
-    >
-      {label.toUpperCase()}
-    </UnderlineLink>
-  );
-};
+const educationLinks = [
+  { href: '/education', label: 'Overview' },
+  { href: '/education/training', label: 'EduPage1' },
+  { href: '/education/resources', label: 'EduPage2' },
+  { href: '/education/youth', label: 'EduPage3' },
+];
 
-const DonateButton = () => {
-  return (
-    <a
-      href='https://www.canadahelps.org/en/dn/9468'
-      target='_blank'
-      className="
-        items-center
-        rounded-md
-        bg-red-600
-        px-4
-        py-2
-        text-sm
-        font-semibold
-        text-white
-        hover:bg-red-700
-        transition-colors
-        whitespace-nowrap
-        mr-4
-      "
-    >
-      Donate Today
-    </a>
-  );
-};
-
-export default function Header({ transparentHeader = true }: { transparentHeader: boolean }) {
+/* -------------------- HEADER -------------------- */
+export default function Header({
+  transparentHeader = true,
+}: {
+  transparentHeader: boolean;
+}) {
   const [isNavbarTransparent, setIsNavbarTransparent] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
+  const [educationOpen, setEducationOpen] = useState(false);
 
   const toggleMenu = () => setIsOpen(!isOpen);
 
@@ -73,41 +50,132 @@ export default function Header({ transparentHeader = true }: { transparentHeader
           : 'bg-white'
       }`}
     >
-      <div className='gradient'>&nbsp;</div>
-      <nav className='flex h-14 items-center justify-between w-full pl-4 pr-1 lg:pl-8 lg:pr-2 relative'>
-        {/* LEFT: Nav links + Hamburger */}
-        <div className='flex items-center space-x-4'>
-          {/* Mobile hamburger */}
-          <div className='lg:hidden'>
-            <button onClick={toggleMenu} className='focus:outline-none'>
+      <div className="gradient">&nbsp;</div>
+
+      <nav className="flex h-14 items-center justify-between w-full pl-4 pr-1 lg:pl-8 lg:pr-2 relative">
+        {/* LEFT */}
+        <div className="flex items-center space-x-4">
+          {/* MOBILE HAMBURGER */}
+          <div className="lg:hidden">
+            <button onClick={toggleMenu}>
               {isOpen ? <List /> : <Menu />}
             </button>
           </div>
 
-          {/* Desktop nav links */}
-          <ul className='hidden lg:flex items-center space-x-10'>
-            {links.map(({ href, label }) => (
-              <li key={`${href}${label}`}>
-                <HeaderItem href={href} label={label} />
+          {/* DESKTOP NAV */}
+          <ul className="hidden lg:flex items-center space-x-10">
+            {links.map((link) => (
+              <li key={link.href}>
+                <UnderlineLink
+                  href={link.href}
+                  className="navbar-link navbar-text-black"
+                >
+                  {link.label.toUpperCase()}
+                </UnderlineLink>
               </li>
             ))}
+
+            {/* EDUCATION (DESKTOP) */}
+            <li className="relative group">
+              <UnderlineLink
+                href="#"
+                className="navbar-link navbar-text-black flex items-center gap-1"
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                EDUCATION
+                <ChevronDown
+                  size={14}
+                  className="transition-transform group-hover:rotate-180"
+                />
+              </UnderlineLink>
+
+              {/* Dropdown */}
+              <div className="absolute left-0 top-full pt-2 hidden group-hover:block">
+                <ul className="bg-white shadow-lg rounded-md overflow-hidden min-w-[200px] z-50">
+                  {educationLinks.map((item) => (
+                    <li
+                      key={item.href}
+                      className="px-4 py-2 hover:bg-gray-100 whitespace-nowrap"
+                    >
+                      <UnderlineLink href={item.href}>
+                        {item.label}
+                      </UnderlineLink>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </li>
           </ul>
         </div>
 
-        {/* RIGHT: Logo + Donate Button */}
-        <div className='flex items-center ml-auto mr-4 lg:mr-3 -mt-0'>
-          <DonateButton></DonateButton>
-          <Image src={logo} alt='Lions Bay SAR Logo' width={50} height={50} />
+        {/* RIGHT */}
+        <div className="flex items-center ml-auto mr-4 lg:mr-3">
+          <a
+            href="https://www.canadahelps.org/en/dn/9468"
+            target="_blank"
+            className="
+              items-center rounded-md
+              bg-red-600 px-4 py-2
+              text-sm font-semibold text-white
+              hover:bg-red-700 transition-colors
+              whitespace-nowrap mr-4
+            "
+          >
+            Donate Today
+          </a>
+          <Image src={logo} alt="Lions Bay SAR Logo" width={50} height={50} />
         </div>
 
         {/* MOBILE DROPDOWN */}
         {isOpen && (
-          <ul className='absolute start-0 top-14 w-full bg-white/[0.50] py-3 lg:hidden'>
-            {links.map(({ href, label }) => (
-              <li key={`${href}${label}`} className='text-center'>
-                <HeaderItem href={href} label={label} />
+          <ul className="absolute start-0 top-14 w-full bg-white/[0.85] py-3 lg:hidden">
+            {links.map((link) => (
+              <li key={link.href} className="text-center">
+                <UnderlineLink
+                  href={link.href}
+                  className="navbar-link navbar-text-black"
+                >
+                  {link.label.toUpperCase()}
+                </UnderlineLink>
               </li>
             ))}
+
+            {/* EDUCATION (MOBILE) */}
+            <li className="flex flex-col items-center">
+              <UnderlineLink
+                href="#"
+                className="navbar-link navbar-text-black flex items-center gap-1"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setEducationOpen(!educationOpen);
+                }}
+              >
+                EDUCATION
+                <ChevronDown
+                  size={16}
+                  className={`transition-transform ${
+                    educationOpen ? 'rotate-180' : ''
+                  }`}
+                />
+              </UnderlineLink>
+
+              {educationOpen && (
+                <ul className="mt-2 w-full">
+                  {educationLinks.map((item) => (
+                    <li key={item.href} className="text-center">
+                      <UnderlineLink
+                        href={item.href}
+                        className="navbar-link navbar-text-black"
+                      >
+                        {item.label.toUpperCase()}
+                      </UnderlineLink>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
           </ul>
         )}
       </nav>
