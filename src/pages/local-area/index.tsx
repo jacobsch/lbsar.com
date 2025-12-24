986a754917997165db16250564dbe39366db6468
Jacob Schwartz <jacobsch@users.noreply.github.com>
2025-12-23 16:49:31 -0800

    Add coming soon placeholder

--- src/pages/local-area/index.tsx
+++ src/pages/local-area/index.tsx
@@ -1,5 +1,7 @@
 import Layout from '$/layout/Layout';
 import Seo from '$/Seo';
+import ArrowLink from '@/components/ui/links/ArrowLink';
+import { RiAlarmWarningFill } from 'react-icons/ri';
 
 /**
  * SVGR Support
@@ -14,6 +16,19 @@ export default function VolunteerPage() {
     <Layout transparentHeader={false}>
       <Seo templateTitle='Volunteer' />
       <main>
+        {/* DELETE THE ENTIRE SECTION WHEN READY, and unused imports */}
+        <section className='bg-white'>
+          <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
+            <RiAlarmWarningFill
+              size={60}
+              className='drop-shadow-glow animate-flicker text-blue-500'
+            />
+            <h1 className='mt-8 text-4xl md:text-6xl'>Coming Soon</h1>
+            <ArrowLink className='mt-4 md:text-lg' href='/'>
+              Back to Home
+            </ArrowLink>
+          </div>
+        </section>
       </main>
     </Layout>
   );
