43d8f096a7789301c65bb0e492ab1e2acc80f6ed
Jacob Schwartz <jacobsch@users.noreply.github.com>
2025-12-29 18:25:18 -0800

    Fixing Aria's mess
    - indentation corrected
    - reverted to single quotes
    - added comment to remind me that the education element is hardcoded and
      not mapped

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -55,91 +55,90 @@ export default function Header({
           : 'bg-white'
       }`}
     >
-      <div className="gradient">&nbsp;</div>
+      <div className='gradient'>&nbsp;</div>
 
-      <nav className="flex h-14 items-center justify-between w-full pl-4 pr-1 lg:pl-8 lg:pr-2 relative">
+      <nav className='flex h-14 items-center justify-between w-full pl-4 pr-1 lg:pl-8 lg:pr-2 relative'>
         {/* LEFT */}
-        <div className="flex items-center space-x-4">
+        <div className='flex items-center space-x-4'>
           {/* MOBILE HAMBURGER */}
-          <div className="lg:hidden">
+          <div className='lg:hidden'>
             <button onClick={toggleMenu}>
               {isOpen ? <List /> : <Menu />}
             </button>
           </div>
 
-{/* DESKTOP NAV */}
-<ul className="hidden lg:flex items-center space-x-10">
-  {links.map(({ href, label }) => (
-    <li key={href}>
-      <UnderlineLink
-        href={href}
-        className="navbar-link navbar-text-black"
-      >
-        {label.toUpperCase()}
-      </UnderlineLink>
-    </li>
-  ))}
+          {/* DESKTOP NAV (EDUCATION HARDCODED) */}
+          <ul className='hidden lg:flex items-center space-x-10'>
+            {links.map(({ href, label }) => (
+              <li key={href}>
+                <UnderlineLink
+                  href={href}
+                  className='navbar-link navbar-text-black'
+                >
+                  {label.toUpperCase()}
+                </UnderlineLink>
+              </li>
+            ))}
 
-  {/* EDUCATION (DESKTOP) */}
-  <li className="relative group">
-    <UnderlineLink
-      href="#"
-      className="navbar-link navbar-text-black flex items-center gap-1"
-      onClick={(e) => {
-        e.preventDefault();
-      }}
-    >
-      EDUCATION
-      <ChevronDown
-        size={14}
-        className="transition-transform group-hover:rotate-180"
-      />
-    </UnderlineLink>
+            {/* EDUCATION (DESKTOP) */}
+            <li className='relative group'>
+              <UnderlineLink
+                href='#'
+                className='navbar-link navbar-text-black flex items-center gap-1'
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                EDUCATION
+                <ChevronDown
+                  size={14}
+                  className='transition-transform group-hover:rotate-180'
+                />
+              </UnderlineLink>
 
-    {/* Dropdown */}
-    <ul className="absolute left-0 mt-3 bg-white shadow-lg rounded-md overflow-hidden opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 min-w-[200px] z-50">
-      {educationLinks.map(({ href, label }) => (
-        <li key={href} className="px-4 py-2 hover:bg-gray-100">
-  <UnderlineLink
-    href={href}
-    className="navbar-link navbar-text-black text-sm"
-  >
-    {label}
-  </UnderlineLink>
-</li>
-
-      ))}
-    </ul>
-  </li>
-</ul>
+              {/* Education Dropdown */}
+              <ul className='absolute left-0 mt-3 bg-white shadow-lg rounded-md overflow-hidden opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 min-w-[200px] z-50'>
+                {educationLinks.map(({ href, label }) => (
+                  <li key={href} className='px-4 py-2 hover:bg-gray-100'>
+                    <UnderlineLink
+                      href={href}
+                      className='navbar-link navbar-text-black text-sm'
+                    >
+                      {label}
+                    </UnderlineLink>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          </ul>
         </div>
 
         {/* RIGHT */}
-        <div className="flex items-center ml-auto mr-4 lg:mr-3">
+        <div className='flex items-center ml-auto mr-4 lg:mr-3'>
           <a
-            href="https://www.canadahelps.org/en/dn/9468"
-            target="_blank"
-            className="
+            href='https://www.canadahelps.org/en/dn/9468'
+            target='_blank'
+            className='
               items-center rounded-md
               bg-red-600 px-4 py-2
               text-sm font-semibold text-white
               hover:bg-red-700 transition-colors
               whitespace-nowrap mr-4
-            "
+            '
           >
             Donate Today
           </a>
-          <Image src={logo} alt="Lions Bay SAR Logo" width={50} height={50} />
+          <Image src={logo} alt='Lions Bay SAR Logo' width={50} height={50} />
         </div>
 
-        {/* MOBILE DROPDOWN */}
+        {/* MOBILE DROPDOWN (EDUCATION HARDCODED) */}
         {isOpen && (
-          <ul className="absolute start-0 top-14 w-full bg-white/[0.85] py-3 lg:hidden">
+          <ul className='absolute start-0 top-14 w-full bg-white/[0.85] py-3 lg:hidden'>
             {links.map((link) => (
-              <li key={link.href} className="text-center">
+              <li key={link.href} className='text-center'>
                 <UnderlineLink
                   href={link.href}
-                  className="navbar-link navbar-text-black"
+                  className='navbar-link navbar-text-black'
                 >
                   {link.label.toUpperCase()}
                 </UnderlineLink>
@@ -147,10 +146,10 @@ export default function Header({
             ))}
 
             {/* EDUCATION (MOBILE) */}
-            <li className="flex flex-col items-center">
+            <li className='flex flex-col items-center'>
               <UnderlineLink
-                href="#"
-                className="navbar-link navbar-text-black flex items-center gap-1"
+                href='#'
+                className='navbar-link navbar-text-black flex items-center gap-1'
                 onClick={(e) => {
                   e.preventDefault();
                   setEducationOpen(!educationOpen);
@@ -166,12 +165,12 @@ export default function Header({
               </UnderlineLink>
 
               {educationOpen && (
-                <ul className="mt-2 w-full">
+                <ul className='mt-2 w-full'>
                   {educationLinks.map((item) => (
-                    <li key={item.href} className="text-center">
+                    <li key={item.href} className='text-center'>
                       <UnderlineLink
                         href={item.href}
-                        className="navbar-link navbar-text-black"
+                        className='navbar-link navbar-text-black'
                       >
                         {item.label.toUpperCase()}
                       </UnderlineLink>
