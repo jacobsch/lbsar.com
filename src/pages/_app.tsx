099eff1d65047cd1b1f03a3054312f39cfb1a0a5
Aria Bahrami <223552048+aria-bahrami@users.noreply.github.com>
2026-01-10 03:27:23 -0800

    Edits

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
-import { AppProps } from 'next/app';
+import type { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
 import '@/styles/header.css';
 import '@/styles/layout.css';
 
-// Note: `Layout` component is called in every page using `np` snippets. If you have consistent layout across all page, you can add it here too
+import Seo from '$/Seo';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Seo />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
