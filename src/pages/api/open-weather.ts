85d991436f3af04d2c6a3ccfbd77988c33aeb109
Aria Bahrami <223552048+aria-bahrami@users.noreply.github.com>
2026-01-16 02:24:56 -0800

    Edit

--- src/pages/api/open-weather.ts
+++ src/pages/api/open-weather.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function hello(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'Bambang' });
+  res.status(200).json({ name: 'OpenWeather' });
 }
